a1dc0cf9e317cf5c053b19a95452fb56af150bf1
Lucas Da Silva <ldasilva@tmmac78.trademe.local>
2016-11-24 17:28:39 +1300

    added dash and rel buttons, functions and styling to make them work

--- popup.js
+++ popup.js
@@ -1,3 +1,22 @@
+//var test1a = document.getElementById('t1a');
+  //  test1a.addEventListener('click', function() {
+    //    window.open("http://www.w3schools.com");
+  //  });
+
+
+//var userInp = prompt("what test environment would you like to use?");
+//window.open("http://www." + userInp + ".trademe.co.nz/");
+
+/*
+var buttons = document.getElementsByTagName('button');
+for (var i = 0; i < buttons.length; i ++) {
+  buttons[i].onclick = function() {
+    window.open("http://www." + this.textContent.replace(" ", "") + ".trademe.co.nz/");
+  };
+}
+*/
+
+// This function enables user to open trademe and preview environments when checkbox is on/off
 var buttons = document.getElementsByTagName('button');
 for (var i = 0; i < buttons.length; i ++) {
   buttons[i].onclick = function() {
@@ -9,3 +28,19 @@ for (var i = 0; i < buttons.length; i ++) {
     }
   };
 }
+
+
+// dashboard button takes user to https://release.trademe.local/Dashboard/TradeMe.TradeMe
+function dashClicker(){
+  window.open("https://release.trademe.local/Dashboard/TradeMe.TradeMe");
+};
+
+document.getElementById('dashClick').addEventListener('click', dashClicker);
+
+
+// release button takes user to https://release.trademe.local/
+function releaseClicker(){
+  window.open("https://release.trademe.local/");
+};
+
+document.getElementById('releaseClick').addEventListener('click', releaseClicker);
