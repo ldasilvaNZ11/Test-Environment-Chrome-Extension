9d9ee37e6cf7ccd41cd5f80a4f0c6378e5d899ba
Lucas Da Silva <ldasilva@tmmac78.trademe.local>
2016-11-28 17:24:24 +1300

    Changes to java script file after removing the word test from each button in the html.

--- popup.js
+++ popup.js
@@ -3,10 +3,10 @@ var buttons = document.getElementsByTagName('button');
 for (var i = 0; i < buttons.length; i ++) {
   buttons[i].onclick = function() {
     if (document.getElementById("previewCheckBox").checked) {
-      window.open("http://preview." + this.textContent.replace(" ", "") + ".trademe.co.nz/");
+      window.open("http://preview.test" + this.textContent.replace(" ", "") + ".trademe.co.nz/");
     }
     else {
-        window.open("http://www." + this.textContent.replace(" ", "") + ".trademe.co.nz/");
+        window.open("http://www.test" + this.textContent.replace(" ", "") + ".trademe.co.nz/");
     }
   };
 }
